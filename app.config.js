d1a2ad03a3429616da09755d5cab9d9c430d2217
Orhan Uzel <153397473+OrhanUzel@users.noreply.github.com>
2026-01-06 00:34:35 +0300

    Fix: Bump iOS deployment target to 16.0 for MLKit

--- app.config.js
+++ app.config.js
@@ -1,112 +1,90 @@
-import 'dotenv/config';
+require('dotenv').config();
 
-export default{
-  "expo": {
-    "name": "Secure QR & Link Scanner",
-    "slug": "secure-qr-link-scanner",
-    "owner": "orhanuzel",
-    "version": "1.2.1",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "extra": {
-      "apiBaseUrl": "https://riskapi.orhanuzel.com.tr",
-      "eas": {
-            "projectId": "3dd1abe1-0e50-4dfa-9930-171cd424ef15"
-
-     
+module.exports = {
+  expo: {
+    name: "Secure QR & Link Scanner",
+    slug: "secure-qr-link-scanner",
+    owner: "orhanuzel",
+    version: "1.2.1",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    extra: {
+      apiBaseUrl: "https://riskapi.orhanuzel.com.tr",
+      eas: {
+        projectId: "3dd1abe1-0e50-4dfa-9930-171cd424ef15"
       },
-      "adUnits": {
-        "androidBanner": process.env.ANDROID_BANNER_ID,
-        "iosBanner": process.env.IOS_BANNER_ID,
-        "androidInterstitial": process.env.ANDROID_INTERSTITIAL_ID,
-        "iosInterstitial": process.env.IOS_INTERSTITIAL_ID,
-        "androidRewarded": process.env.ANDROID_REWARDED_ID,
-        "iosRewarded": process.env.IOS_REWARDED_ID,
-        "androidRewardedInterstitial": process.env.ANDROID_REWARDED_INTERSTITIAL_ID,
-        "iosRewardedInterstitial": process.env.IOS_REWARDED_INTERSTITIAL_ID,
-        "androidNative": process.env.ANDROID_NATIVE_ID,
-        "iosNative": process.env.IOS_NATIVE_ID
+      adUnits: {
+        androidBanner: process.env.ANDROID_BANNER_ID,
+        iosBanner: process.env.IOS_BANNER_ID,
+        androidInterstitial: process.env.ANDROID_INTERSTITIAL_ID,
+        iosInterstitial: process.env.IOS_INTERSTITIAL_ID,
+        androidRewarded: process.env.ANDROID_REWARDED_ID,
+        iosRewarded: process.env.IOS_REWARDED_ID,
+        androidRewardedInterstitial: process.env.ANDROID_REWARDED_INTERSTITIAL_ID,
+        iosRewardedInterstitial: process.env.IOS_REWARDED_INTERSTITIAL_ID,
+        androidNative: process.env.ANDROID_NATIVE_ID,
+        iosNative: process.env.IOS_NATIVE_ID
       }
     },
-    "splash": {
-      "image": "./assets/splash-icon.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
     },
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.orhanuzel.secureqrlinkscanner",
-      "infoPlist": {
-        "ITSAppUsesNonExemptEncryption": false
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.orhanuzel.secureqrlinkscanner",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false
       }
     },
-    "android": {
-      "versionCode": 10,
-
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    android: {
+      versionCode: 10,
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
       },
-      "edgeToEdgeEnabled": true,
-      "package": "com.orhanuzel.secureqrlinkscanner",
-      "intentFilters": [
+      edgeToEdgeEnabled: true,
+      package: "com.orhanuzel.secureqrlinkscanner",
+      intentFilters: [
         {
-          "action": "VIEW",
-          "data": [
-            {
-              "scheme": "http"
-            },
-            {
-              "scheme": "https"
-            }
+          action: "VIEW",
+          data: [
+            { scheme: "http" },
+            { scheme: "https" }
           ],
-          "category": [
-            "BROWSABLE",
-            "DEFAULT"
-          ]
+          category: ["BROWSABLE", "DEFAULT"]
         },
         {
-          "action": "SEND",
-          "category": [
-            "DEFAULT"
-          ],
-          "data": [
-            {
-              "mimeType": "text/plain"
-            }
-          ]
+          action: "SEND",
+          category: ["DEFAULT"],
+          data: [{ mimeType: "text/plain" }]
         },
         {
-          "action": "SEND",
-          "category": [
-            "DEFAULT"
-          ],
-          "data": [
-            {
-              "mimeType": "image/*"
-            }
-          ]
+          action: "SEND",
+          category: ["DEFAULT"],
+          data: [{ mimeType: "image/*" }]
         }
       ]
     },
-    "scheme": "secureqrlinkscanner",
-    "web": {
-      "favicon": "./assets/favicon.png"
+    scheme: "secureqrlinkscanner",
+    web: {
+      favicon: "./assets/favicon.png"
     },
-    "plugins": [
+    plugins: [
       "./plugins/withUnityAds/app.plugin.js",
       "./plugins/withShareIntent/app.plugin.js",
       [
         "expo-build-properties",
         {
-          "ios": {
-            "useFrameworks": "static",
-            "deploymentTarget": "16.0",
-            "extraPods": [
+          ios: {
+            useFrameworks: "static",
+            deploymentTarget: "16.0",
+            extraPods: [
               {
-                "name": "Google-Mobile-Ads-SDK",
-                "version": "11.13.0"
+                name: "Google-Mobile-Ads-SDK",
+                version: "11.13.0"
               }
             ]
           }
@@ -116,9 +94,9 @@ export default{
       [
         "react-native-google-mobile-ads",
         {
-          "androidAppId": process.env.ADMOB_ANDROID_APP_ID,
-          "iosAppId": process.env.ADMOB_IOS_APP_ID,
-          "skAdNetworkItems": [
+          androidAppId: process.env.ADMOB_ANDROID_APP_ID,
+          iosAppId: process.env.ADMOB_IOS_APP_ID,
+          skAdNetworkItems: [
             "cstr6suwn9.skadnetwork",
             "4fzdc2evr5.skadnetwork",
             "xga6mpmplv.skadnetwork",
@@ -129,4 +107,4 @@ export default{
       "expo-secure-store"
     ]
   }
-}
+};
